8f75bb0cb0c21711d56d23c2c7b843eca3d705bd
Ehsan Ali <ehsan.ali@invozone.com>
2021-05-18 20:13:58 +0500

    - docs commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  configFile: 'assets/config/config.json'
+  // baseURL:"http://localhost:8060",
+  baseURL:"http://18.206.59.178:8060",
+  authURL:'https://preprodjv.dba.ma',
+  configFile: 'assets/config/config.dev.json'
 };
